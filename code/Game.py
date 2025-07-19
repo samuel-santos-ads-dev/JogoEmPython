f3b4c7f07cf459e83308445a12823a62d29af3cd
Samuel Santos <samuelsantossamukacpu@gmail.com>
2025-07-19 19:49:28 -0300

    Subindo as mídias e configurando parte do menu

--- code/Game.py
+++ code/Game.py
@@ -1,13 +1,17 @@
 import pygame
 
+from code.Const import TEL_HEIGHT, TEL_WIDTH
 from code.Menu import Menu
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.tela = pygame.display.set_mode(size = (700, 480))
+        self.tela = pygame.display.set_mode(size = (TEL_WIDTH, TEL_HEIGHT))
 
     def run(self):
+        pygame.mixer_music.load('./asset/Menu.mp3')
+        pygame.mixer_music.play(-1)
+
         while (True):
             menu = Menu(self.tela)
             menu.run()
--- code/Menu.py
+++ code/Menu.py
@@ -1,6 +1,13 @@
+import pygame
+
+
 class Menu:
     def __init__(self, tela):
         self.tela = tela
+        self.surf = pygame.image.load('./asset/MenuBg.png')
+        self.rect = self.surf.get_rect(left = 0, top = 0)
 
     def run(self, ):
+        self.tela.blit(source=self.surf, dest=self.rect)
+        pygame.display.flip()
         pass
